c0dae815ac0531f114a8710c415d1287d95b107f
Albert Lu <albertlu@alum.mit.edu>
2010-10-25 21:38:52 +0800

    first commit

---
+++ simple_mes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleMes::Application.config.secret_token = 'b569a5164f67e5fb8e330d2cd4641e89e151e2b76708931cfb96278960d8a6c67f09e4671fc391a94da8be008e5b83feda646fed63912d0ed53956c1fc617d11'
